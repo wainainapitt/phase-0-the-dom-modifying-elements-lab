cfc0abb67562079937ef9da9562b86d3e8b1906b
peter <pngugi.wainaina@gmail.com>
2023-08-06 21:48:02 +0300

    dom lab submitted

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+const body = document.createTextNode("Hello, World!");
+newHeader.appendChild(body);
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Peter is the champion"
